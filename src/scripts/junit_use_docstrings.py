03eaa6145861320fcf799de56bd6d6d16cf645b2
Scott Poore <spoore@redhat.com>
2015-06-02 20:59:47 -0500

    junit_use_docstrings:  add script
    adding a first draft script for converting the test
    case name in junit output xml to use docstrings.
    
    Still needs error handling, arg passing, etc.
    
    - Adding support for multiple testsuites
    - Adding cli arg parsing for the following:
      -h, --help            show this help message and exit
      -i INPUT_FILE, --input_file INPUT_FILE
                            Path to Junit XML Input file
      -o OUTPUT_FILE, --output_file OUTPUT_FILE
                            Path to Junit XML Output file
      -t TEAM, --team TEAM  Team Name to use in classname
      -s SUITE, --suite SUITE
                            Suite Name to use in classname
      -p PACKAGE, --package PACKAGE
                            Package Name to use in classname
    
    - Better error handling than previous patchsets.
    - Better variable naming and parsing for defaults.
    - Better error message for ImportError.
    
    Change-Id: I98b000d39532abb95ea858d57db14a63b7276206
    Signed-off-by: Scott Poore <spoore@redhat.com>

---
+++ src/scripts/junit_use_docstrings.py
@@ -0,0 +1,80 @@
+import os
+import argparse
+import importlib
+import xml.etree.ElementTree as ET
+
+parser = argparse.ArgumentParser(description="Convert Junit XML test case names")
+parser.add_argument('-i', '--input_file', help='Path to Junit XML Input file')
+parser.add_argument('-o', '--output_file', help='Path to Junit XML Output file')
+parser.add_argument('-t', '--team', help='Team Name to use in classname')
+parser.add_argument('-s', '--suite', help='Suite Name to use in classname')
+parser.add_argument('-p', '--package', help='Package Name to use in classname')
+args = parser.parse_args()
+input_file = args.input_file or '/tmp/junit.xml'
+output_file = args.output_file or '/tmp/newjunit.xml'
+team = args.team or 'RH'
+team = team.upper()
+suite = args.suite or None
+pkg = args.package or None
+
+# print args
+# print "INPUT = " + input_file
+# print "OUTPUT = " + output_file
+# print "TEAM = " + team
+# print "SUITE = " + str(suite)
+
+tree = ET.parse(input_file)
+for ts in tree.iter('testsuite'):
+    for tc in ts.iter('testcase'):
+        # print tc.tag, tc.attrib
+        # print tc.attrib['classname']
+        # print tc.attrib['name']
+
+        class_list = tc.attrib['classname'].split('.')[::-1]
+        class_name = class_list[0] if class_list[0:] else ""
+        module_name = class_list[1] if class_list[1:] else ""
+        suite_name = class_list[2] if class_list[2:] else ""
+        src_name = class_list[3] if class_list[3:] else ""
+        pkg_name = class_list[4] if class_list[4:] else ""
+
+        if pkg_name is not "":
+            pkg_name = pkg_name.replace('-', '_')
+
+        if pkg is not None:
+            pkg_name = pkg
+        elif pkg_name is "":
+            print "Unknown package.  Please specify on the command line"
+            exit()
+
+        if suite is not None:
+            suite_name = suite
+        elif suite_name is "":
+            suite_name = os.path.basename(os.getcwd())
+
+        if suite is not None:
+            suite_nat = suite.replace('_', ' ').title()
+        else:
+            suite_nat = suite_name.replace('_', ' ').title()
+
+        method_name = tc.attrib['name']
+        # print suite_nat + "::" + module_name + "::" + class_name + "::" + method_name + "\n"
+
+        try:
+            my_mod = importlib.import_module(pkg_name + '.' + suite_name + '.' + module_name)
+        except ValueError:
+            print "Missing information for importing module to find docstring"
+            exit()
+        except ImportError, e:
+            print(e)
+            print "Invalid Module Path: %s" % pkg_name + '.' + suite_name + '.' + module_name
+            exit()
+
+        my_class = getattr(my_mod, class_name)
+        my_method = getattr(my_class, method_name)
+        # print my_method.__doc__
+
+        tc.attrib['name'] = team + "-TC: " + suite_nat + ": " + my_method.__doc__.strip()
+
+tree.write(output_file)
+
+print "New file written to " + output_file
