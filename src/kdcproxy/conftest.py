c743b3d3b6b104fc7f798180e2bbc150ad2e9e63
Abhijeet Kasurde <akasurde@redhat.com>
2015-11-24 16:46:28 +0530

    Added testcases for KdcProxy
    - Removed pytest.ini
    - Renamed kdcproxy to test_0001_kdcproxy
    - Moved all helper functions to lib.py
    - Update client install command line in qe_install
    - Added docstring for functions and testcases
    - Update testcase names in brief
    - Fixed all pylint and pep8 warnings
    - Used 'as' in error handling
    - More lenient about 80 char limit
    - Updated code for kpasswd_server setting in krb5.conf
    - Moved variables in conftest
    - Added new testcases
    - Using class_setup instead of setup_session
    
    Change-Id: I5c6fc2517155b1141913602337df3c2e96149235
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

---
+++ src/kdcproxy/conftest.py
@@ -0,0 +1,35 @@
+# pylint: disable=R0801
+""" KDCProxy Conftest
+conftest to setup required fixtures needed by tests:
+- config for multihost plugin
+"""
+
+import pytest
+from ipa_pytests.qe_install import setup_client, setup_replica, setup_master
+from ipa_pytests.qe_class import multihost
+from ipa_pytests.qe_class import qe_extra_print
+from ipa_pytests.qe_class import qe_use_class_setup
+
+
+def pytest_namespace():
+    """ Define the number of test host roles using namespace hook """
+    return {'num_replicas': 1,
+            'num_clients': 1,
+            'num_others': 0}
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_session(request, multihost):
+    """ Setup session """
+    try:
+        setup_master(multihost.master)
+        setup_replica(multihost.replica, multihost.master)
+        setup_client(multihost.client, multihost.master)
+    except StandardError as errval:
+        print("Error in setup_session %s" % (str(errval.args[0])))
+        pytest.skip("setup_session_skip")
+
+    def teardown_session():
+        """ define fixture for session level teardown """
+        pass
+    request.addfinalizer(teardown_session)
