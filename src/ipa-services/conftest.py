82ffea9a28f3010685719baac125357515569676
Nikhil Dehadrai <ndehadra@redhat.com>
2016-01-12 18:10:00 +0530

    Added Test case for RFE:1204504 The the RFE is about verifiying bug#1204504 that checks for ability to verify bug by adding services on client machine and ipa master server. It also uninstalls the ipa server and client as part of cleanup.
    PATCH 2:
    1. Modified the setup.py to remove tab and spaces , also added docstring to it.
    2. Modified test_001_service_add_host.py based on the code review comments and removed the uninstallation of server/client section as per discussion since it is not required in this case.
    
    PATCH 3:
    1. Removed mh_cfg.yaml as per suggestions.
    2. Moved ipa-services to src.
    3. Renamed the python execution script name and class used inside the script as per RFE name for future tracking.
    4. Have retained the ipa-services inside setup.py as per the discussion.
    
    Change-Id: I9e5ec9e5234cc797cc977eaeaaee9604f8a562e7
    Signed-off-by: Nikhil Dehadrai <ndehadra@redhat.com>

---
+++ src/ipa-services/conftest.py
@@ -0,0 +1,32 @@
+""" verify_client_version Conftest
+conftest to setup required fixtures needed by tests:
+- config for multihost plugin
+"""
+
+
+import pytest
+from ipa_pytests.qe_install import setup_client, setup_master
+
+
+def pytest_namespace():
+    """ Define the number of test host roles using namespace hook """
+    return {'num_replicas': 0,
+            'num_clients': 1,
+            'num_others': 0
+            }
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_session(request, multihost):
+    try:
+        multihost.client = multihost.clients[0]
+        setup_master(multihost.master)
+        setup_client(multihost.client, multihost.master)
+    except StandardError, errval:
+        print str(errval.args[0])
+        pytest.skip("setup_session_skip")
+
+    def teardown_session():
+        """ define fixture for session level teardown """
+        pass
+    request.addfinalizer(teardown_session)
+
