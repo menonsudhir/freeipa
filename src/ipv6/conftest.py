3d2da73289705af56beeaa1709654145d3497f48
Pavel Picka <ppicka@redhat.com>
2016-05-06 18:39:07 +0200

    Functional ipv6 test
    Change-Id: I777ec0fad39195027d4e063da4cff2f76b6c5a85
    Signed-off-by: Pavel Picka <ppicka@redhat.com>

---
+++ src/ipv6/conftest.py
@@ -0,0 +1,41 @@
+""" IPV6 Conftest
+conftest to setup required fixtures needed by tests:
+- config for multihost plugin
+- setup session scoped setup and teardown
+- must include import for multihost fixture if using multihost plugin
+- must include import for qe_use_class_setup fixture if test class will have
+  class_setup and class_teardown methods.
+"""
+
+import pytest
+from ipa_pytests.qe_class import multihost  # pylint: disable=unused-import
+from ipa_pytests.qe_class import qe_use_class_setup  # pylint: disable=unused-import
+from ipa_pytests.functional_services import setup_lib
+
+
+def pytest_namespace():
+    """ Define the number of test host roles using namespace hook """
+    return {'num_replicas': 1,
+            'num_clients': 1,
+            'num_others': 0}
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_session(request, multihost):
+    """ define fixture for session level setup """
+    # Defining convenience variables for multihost fixture
+    # for single replica and client
+    multihost.replica = multihost.replicas[0]
+    multihost.client = multihost.clients[0]
+
+    tp = setup_lib.TestPrep(multihost)
+    try:
+        tp.setup()
+    except StandardError, errval:
+        print str(errval.args[0])
+        pytest.skip("setup_session_skip")
+
+    def teardown_session():
+        """ define fixture for session level teardown """
+        tp.teardown()
+    request.addfinalizer(teardown_session)
