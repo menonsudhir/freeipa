0813e207897376cd0aa2669a9a2568f2a30f47ba
Scott Poore <spoore@redhat.com>
2016-07-05 11:40:46 -0500

    server-install: bz1351276
    adding test_bugcheck_0003 to handle simple miscellaneous master
    install bug checks after installation.
    
    Change-Id: Id343b5a4dc6b9bf243fd0d0de7c149f4b6869c0b
    Signed-off-by: Scott Poore <spoore@redhat.com>

---
+++ src/server-install/test_bugcheck_0003.py
@@ -0,0 +1,28 @@
+"""
+TestCases for miscellaneous master install bugs
+- These cases should be configured for master only install
+- helps handle simple install bug checks post-install
+"""
+import pytest
+from ipa_pytests.qe_install import setup_master, uninstall_server
+from ipa_pytests.qe_class import multihost
+from ipa_pytests.qe_class import qe_use_class_setup
+
+
+class TestMasterInstallBugs(object):
+    """ Test Class """
+    def class_setup(self, multihost):
+        """ Setup for class """
+        setup_master(multihost.master)
+
+    def test_0001_bz1351276(self, multihost):
+        """
+        Testcase to verify bz1205264
+        """
+        ipa_ca_fqdn = 'ipa-ca.' + multihost.master.domain.name
+        multihost.master.qerun(['dig', '+short', ipa_ca_fqdn],
+                               exp_output=multihost.master.ip)
+
+    def class_teardown(self, multihost):
+        """ Full suite teardown """
+        uninstall_server(multihost.master)
