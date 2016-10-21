ec7793acb5ab498b66aba582c054072e2623a7f0
Sudhir Menon <sumenon@redhat.com>
2016-10-21 14:40:21 +0530

    Automation bz1369414 - ipa server-del fails with Python stack trace
    1. Modified test_0001_cleanup.py as per review comments
    2. Modified conftest.py as per review comments
    
    Change-Id: I54b9a79c247247c2e7250f2d3eab29574cc2f7bc

---
+++ src/server-role/conftest.py
@@ -0,0 +1,30 @@
+""" verify_client_version Conftest
+conftest to setup required fixtures needed by tests:
+- config for multihost plugin
+"""
+
+import pytest
+from ipa_pytests.qe_install import setup_master, uninstall_server
+
+
+def pytest_namespace():
+    """ Define the number of test host roles using namespace hook """
+    return {'num_replicas': 0,
+            'num_clients': 0,
+            'num_masters': 1,
+            'num_others': 0
+            }
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_session(request, multihost):
+    try:
+        setup_master(multihost.master)
+    except StandardError as errval:
+        print str(errval.args[0])
+        pytest.skip("setup_session_skip")
+
+    def teardown_session():
+        """ define fixture for session level teardown """
+        uninstall_server(multihost.master)
+    request.addfinalizer(teardown_session)
---
+++ src/server-role/test_0001_bugcheck.py
@@ -0,0 +1,34 @@
+"""
+Overview:
+SetUp Requirements:
+IPA Server configured on RHEL7.2
+"""
+
+import pytest
+from ipa_pytests.qe_class import multihost
+
+
+class Testserverdel(object):
+
+    """ Test Class """
+    def class_setup(self, multihost):
+        """ Setup for class """
+        print "\nClass Setup"
+        print "MASTER: ", multihost.master.hostname
+
+    def test_0001_ipaserverdel(self, multihost):
+        """
+        IDM-IPA-TC: ipa server-del: ipa server-del gives
+                    error when run without Server name value
+                    bz1369414
+        """
+        multihost.master.kinit_as_admin()
+        cmd = multihost.master.run_command(['ipa', 'server-del'],
+                                           stdin_text='\r',
+                                           raiseonerr=False)
+        print cmd.stdout_text
+        exp_output = "ipa: ERROR: 'cn' is required"
+        if exp_output in cmd.stderr_text:
+            print "Server name needs to be specified"
+        else:
+            pytest.xfail("FAIL")
