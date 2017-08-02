7c608e63da3f6ee39279a8cd61b0663fe4441dc5
Nikhil Dehadrai <ndehadra@redhat.com>
2017-08-02 12:53:19 +0530

    sssd_container: Correction of sssd-container test-suite.
    Patch1:
       1) With addition of new functions of ipa-server-docker and
          ipa-client-docker installation/uninstallation, updated the
          sssd-container likewise.
       2) Privatebin:
          https://privatebin-it-iso.int.open.paas.redhat.com/?c641eb18d026bea2#GyjBiICeHa5fPJvus8pJO3JF7SJo+p//bMhDczmxBm4=
    
    Change-Id: I7ad0a0010bee72b47f0af8fca3ecdccd1600ceb7
    Signed-off-by: Nikhil Dehadrai <ndehadra@redhat.com>

--- src/sssd_container/conftest.py
+++ src/sssd_container/conftest.py
@@ -1,7 +1,5 @@
-"""
-Test cases for sssd-container with IPA.
-"""
 import pytest
+from ipa_pytests.qe_install import setup_master_docker, uninstall_master_docker
 from ipa_pytests.qe_class import multihost
 
 
@@ -9,16 +7,24 @@ def pytest_namespace():
     """ Define the number of test host roles using namespace hook """
     return {'num_replicas': 0,
             'num_clients': 1,
-            'num_others': 0}
+            'num_others': 0,
+            'num_ads': 0}
 
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_session(request, multihost):
-    multihost.client = multihost.clients[0]
+    try:
+        setup_master_docker(multihost.master)
+        print("setup_master_docker")
+        multihost.client = multihost.clients[0]
+
+
+    except StandardError as errval:
+        print("Error in setup_session %s" % (str(errval.args[0])))
+        pytest.skip("setup_session_skip")
 
     def teardown_session():
         """ define fixture for session level teardown """
-        print('This is teardown session')
-        contents = multihost.client.get_file_contents('/etc/resolv.conf.backup')
-        multihost.client.put_file_contents('/etc/resolv.conf', contents)
+        print("This is teardown")
+        uninstall_master_docker(multihost.master)
     request.addfinalizer(teardown_session)
--- src/sssd_container/test_ipa_sssd_container.py
+++ src/sssd_container/test_ipa_sssd_container.py
@@ -4,15 +4,11 @@ Test cases for SSSD container Testing with IPA.
 Note: Make sure PermitRootLogin and PasswordAuthentication is
 enabled on atomic host client machine in order to run the below
 tests.
-Also, we use a static IPA server : 10.76.33.240 to bypass CI 
-limitation of not simultaneously supporting RHEL Non-Atomic system
-and RHEL Atomic host during provisioning.Credentials to
-IPA master: root / Secret123.
 ###############################################################
 '''
 import pytest
-from ipa_pytests.shared.utils import service_control
-
+from ipa_pytests.shared.utils import *
+from ipa_pytests.qe_install import setup_client_docker, uninstall_client_docker
 
 class Testmaster(object):
     """ Test Class """
@@ -22,74 +18,38 @@ class Testmaster(object):
         print"MASTER: ", multihost.master.hostname
         print"CLIENT: ", multihost.client.hostname
 
-    def test_0001_Prerequiste(self, multihost):
-        """Taking back up of resolv.conf in order to update it with
-        IPA server details. Also creating client installation parametr file,
-        this file contains the IPA server details and will be invoked while
-        installing IPA client using SSSD container."""
-        multihost.client.log.info("Taking backup of /etc/resolv.conf")
-        contents = multihost.client.get_file_contents('/etc/resolv.conf')
-        multihost.client.put_file_contents('/etc/resolv.conf.backup', contents)
-        name_server = ('nameserver ' + multihost.master.ip)
-        multihost.client.put_file_contents('/etc/resolv.conf', name_server)
-        install_options = ('--server=' + multihost.master.hostname +
-                           '\n--domain=testrelm.test'
-                           '\n--principal admin --password Secret123'
-                           '\n--force-join')
-        multihost.client.put_file_contents('/etc/sssd/ipa-client-install-options',
-                                           install_options)
+    def test_0001_Install_Client(self, multihost):
+        """Install IPA client using sssd-container"""
+        print("Runing ipa_client_docker")
+        client = multihost.client
+        master = multihost.master
+        setup_client_docker(client, master)
 
-    def test_0002_Setup_Install_Client(self, multihost):
-        """Installing IPA client with SSSD container image.
-        Also verifying if client installation was successful."""
-        check1 = multihost.client.run_command('atomic install rhel7/sssd',
-                                              raiseonerr=False)
-        if check1.returncode == 0:
-            print "IPA client install successfull."
-        else:
-            pytest.fail("IPA client install not successful.")
-        if not multihost.client.transport.file_exists('/etc/systemd/system/sssd.service'):
-            pytest.fail('SSSD service file not found, kindly debug')
-        multihost.client.qerun(['systemctl', 'restart', 'sssd'],
-                               exp_returncode=0)
-
-    def test_0003_Kinit_works_for_Client(self, multihost):
+    def test_0002_Kinit_works_for_Client(self, multihost):
         """Verifying that Klist and Kinit command works."""
-        cmd1 = multihost.client.run_command(['docker', 'exec', '-i', 'sssd',
-                                             'kinit', 'admin'],
-                                            stdin_text='Secret123',
-                                            set_env=False,
-                                            raiseonerr=False)
-        print(cmd1.stdout_text)
-        assert cmd1.returncode == 0
-        multihost.client.qerun(['docker', 'exec', 'sssd', 'klist'],
-                               exp_returncode=0)
-        multihost.client.qerun(['docker', 'exec', 'sssd', 'kdestroy'],
-                               exp_returncode=0)
-        # Again verifying that new ticket gets generated after kdestroy.
-        cmd2 = multihost.client.run_command(['docker', 'exec', '-i', 'sssd',
-                                             'kinit', 'admin'],
-                                            stdin_text='Secret123',
-                                            set_env=False,
-                                            raiseonerr=False)
-        print(cmd2.stdout_text)
-        assert cmd2.returncode == 0
-        multihost.client.qerun(['docker', 'exec', 'sssd', 'klist'],
-                               exp_returncode=0)
+        client = multihost.client
+        container = 'sssd'
+        docker_get_version(client, container)
+        docker_kinit_as_admin(client, container)
+        docker_klist(client, container)
+        docker_kdestroy(client, container)
+        docker_klist(client, container)
+        docker_kinit_as_admin(client, container)
+        docker_klist(client, container)
 
-    def test_0004_SSH_Client(self, multihost):
+    def test_0003_SSH_Client(self, multihost):
         """Verifying if ssh access is poosible for client"""
         cmd1 = 'ssh -o GSSAPIAuthentication=yes admin@'
         cmd = multihost.client.run_command(cmd1 + '`hostname`' + ' whoami',
-                                            stdin_text='Secret123',
-                                            raiseonerr=False)
+                                           stdin_text='Secret123',
+                                           raiseonerr=False)
         assert cmd.returncode == 0
         print(cmd.stdout_text)
 
-    def test_0005_Uninstall_Client(self, multihost):
-        """Testing if IPA client can be uninstalled."""
-        multihost.client.qerun(['atomic', 'uninstall', 'rhel7/sssd'],
-                               exp_returncode=0)
+    def test_0004_Uninstall_Client(self, multihost):
+        """ Uninstall Client"""
+        client = multihost.client
+        uninstall_client_docker(client)
 
     def class_teardown(self, multihost):
         pass
