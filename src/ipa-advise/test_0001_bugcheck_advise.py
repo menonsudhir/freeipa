789ba4e9f03f15b85219b926ad0a5df199db3d2d
Anuja More <amore@redhat.com>
2017-08-31 18:56:54 +0530

    Automation for bz1353899
    Patch1:
    1. Automation for bz1353899
    
    Patch2:
    1. Removed unused library
    
    Patch3:
    1. Modified test_0001_bugcheck_advise.py for pep8
    
    Patch4:
    1. Modified mh_cfg.yaml for hostname
    
    Patch5:
    1. Modified test_0001_cleanup.py by using uninstall_server and uninstall_client
    
    Patch6:
    1. Modified test_0001_bugcheck_advise.py removed unused data
    
    Patch7:
    1. Modified test_0001_cleanup.py by using comma and multihost.master and multihost.client
    
    Patch8:
    1. Modified test_0001_cleanup.py multihost.client.uninstall_client(multihost.client) removed
    2. Modified test_0001_bugcheck_advise.py declaration of exp_output1 is removed and overview is changed
    3. Modified mh_cfg.yaml client part is removed
    
    http://pastebin.test.redhat.com/513048
    
    Change-Id: I94abd77757cd0153df7649f3647f0ec12bce348d
    Signed-off-by: Anuja More <amore@redhat.com>

---
+++ src/ipa-advise/test_0001_bugcheck_advise.py
@@ -0,0 +1,36 @@
+"""
+Overview:
+Test suite to verify ipa-advise output
+"""
+
+import pytest
+from ipa_pytests.qe_class import multihost
+from ipa_pytests.qe_class import qe_use_class_setup
+
+
+class Testipaadvise(object):
+
+    """ Test Class """
+    def class_setup(self, multihost):
+        """ Setup for class """
+        print "\nClass Setup"
+        print "MASTER: ", multihost.master.hostname
+
+    def test_0001_ipa_advise(self, multihost):
+        """
+        Test to verify Bugzilla 1353899 - ipa-advise
+         object of type 'type' has no len()
+        """
+        realm = multihost.master.domain.realm
+        multihost.master.kinit_as_admin()
+        exp_output = "Instructions for configuring a system"
+        var1 = multihost.master.run_command(['ipa-advise'])
+
+        if var1.stdout_text.find(exp_output):
+            print "test_0001_ipa_advise verified"
+        else:
+            print "bugzilla 1353899 found"
+
+    def class_teardown(self, multihost):
+        """ Class Teardown """
+        pass
---
+++ src/ipa-advise/test_0001_cleanup.py
@@ -0,0 +1,19 @@
+"""
+This is a cleanup module to undo everything the test did.
+It should uninstall and reset configurations.
+"""
+import pytest
+from ipa_pytests.qe_class import multihost
+from ipa_pytests.qe_install import uninstall_server
+
+
+class TestCleanup(object):
+    """ Cleanup Class """
+    def test_setup(self, multihost):
+        """ class level setup...should do nothing here """
+        pass
+
+    def test_teardown(self, multihost):
+        """ Full suite teardown """
+        multihost.master.kinit_as_admin()
+        uninstall_server(multihost.master)
