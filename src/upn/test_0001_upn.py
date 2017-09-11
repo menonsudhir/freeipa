e6474287e81dbbb3700ede123aaa7df4508bd4cb
Xiyang Dong <xdong@redhat.com>
2017-09-11 16:07:01 -0400

    FREEIPA-67 Fix upn-pytest test failures
    Change-Id: Ifaef2d72196886ad72c87540f567ce6e24272465

--- src/upn/test_0001_upn.py
+++ src/upn/test_0001_upn.py
@@ -3,6 +3,7 @@ This covers the test cases for upn feature
 """
 from ipa_pytests.qe_class import multihost
 from ipa_pytests.qe_class import qe_use_class_setup
+from ipa_pytests.shared.utils import (service_control)
 from ipa_pytests.shared.utils import (disable_dnssec, dnsforwardzone_add,
                                       add_dnsforwarder, sssd_cache_reset)
 from ipa_pytests.qe_install import adtrust_install
@@ -36,10 +37,11 @@ class TestUPN(object):
 
         dnsforwardzone_add(multihost.master, forwardzone, ad1.ip)
 
-        add_dnsforwarder(ad1, domain, multihost.master.external_ip)
+        add_dnsforwarder(ad1, domain, multihost.master.ip)
 
         cmd = multihost.master.run_command('dig +short SRV _ldap._tcp.' +
-                                           forwardzone, raiseonerr=False)
+                                           forwardzone, 
+                                           raiseonerr=False)
         print cmd.stdout_text, cmd.stderr_text
         if ad1.hostname in cmd.stdout_text:
             print("dns resolution passed for ad domain")
@@ -103,24 +105,6 @@ class TestUPN(object):
         realm = multihost.master.domain.realm
 
         multihost.master.kinit_as_admin()
-
-        add_dnsforwarder(ad1, domain, multihost.master.external_ip)
-        cmd = multihost.master.run_command('dig +short SRV _ldap._tcp.' +
-                                           forwardzone, raiseonerr=False)
-        print cmd.stdout_text, cmd.stderr_text
-        if ad1.hostname in cmd.stdout_text:
-            print("dns resolution passed for ad domain")
-        else:
-            pytest.xfail("dns resolution failed for ad domain")
-        cmd = multihost.master.run_command('dig +short SRV @' + ad1.ip +
-                                           ' _ldap._tcp.' + domain,
-                                           raiseonerr=False)
-        print cmd.stdout_text, cmd.stderr_text
-        if domain in cmd.stdout_text:
-            print("dns resolution passed for ipa domain")
-        else:
-            pytest.xfail("dns resolution failed for ipa domain")
-
         cmd = multihost.master.run_command(['ipa', 'trust-add', forwardzone,
                                             '--admin', ad1.ssh_username,
                                             '--type=ad',
@@ -176,7 +160,7 @@ class TestUPN(object):
         multihost.master.run_command(['ipa', 'trust-fetch-domains', forwardzone],
                                      raiseonerr=False)
         cmd = multihost.master.run_command(['ipa', 'trust-show', forwardzone],
-                                     raiseonerr=False)
+                                           raiseonerr=False)
         print cmd.stdout_text, cmd.stderr_text
         if upn_suffix in cmd.stdout_text:
             print "UPN suffix is seen as expected"
@@ -200,6 +184,28 @@ class TestUPN(object):
         domain = multihost.master.domain.name
         realm = multihost.master.domain.realm
 
+        # Add and set UPN of the ad user
+        cmd = ad1.run_command('dsget user "cn=%s,cn=Users,dc=%s,dc=%s"' %
+                             (aduser, forwardzone.split(".")[0], forwardzone.split(".")[1]),
+                              raiseonerr=False)
+        print ("dsget return code is: %s" % cmd.returncode)
+        print cmd.stdout_text, cmd.stderr_text
+        if cmd.returncode == 1:
+            cmd = ad1.run_command('dsrm "cn=%s,cn=Users,dc=%s,dc=%s" -noprompt' %
+                                 (aduser, forwardzone.split(".")[0], forwardzone.split(".")[1]),
+                                  raiseonerr=False)
+            print ("dsrm return code is: %s" % cmd.returncode)
+            print cmd.stdout_text, cmd.stderr_text
+        cmd = ad1.run_command('dsadd user "cn=%s,cn=Users,dc=%s,dc=%s" -pwd %s -upn %s' %
+                             (aduser, forwardzone.split(".")[0], forwardzone.split(".")[1], aduser_pwd, upn_full),
+                              raiseonerr=False)
+        print ("dsadd return code is: %s" % cmd.returncode)
+        print cmd.stdout_text, cmd.stderr_text
+        if "dsadd succeeded" in cmd.stdout_text:
+            print "AD user add and UPN set successfully"
+        else:
+            pytest.xfail("AD user add and UPN failed")
+
         multihost.master.kinit_as_admin()
         cmd = multihost.master.run_command(['ipa', 'trust-add', forwardzone,
                                             '--admin', ad1.ssh_username,
@@ -215,36 +221,22 @@ class TestUPN(object):
         multihost.master.run_command(['ipa', 'trust-fetch-domains', forwardzone],
                                      raiseonerr=False)
         cmd = multihost.master.run_command(['ipa', 'trust-show', forwardzone],
-                                     raiseonerr=False)
+                                           raiseonerr=False)
         print cmd.stdout_text, cmd.stderr_text
         if upn_suffix in cmd.stdout_text:
             print "UPN suffix is seen as expected"
         else:
             pytest.xfail("UPN suffix is not seen as expected")
-        # Add and set UPN of the ad user
-        cmd = ad1.run_command('dsget user "cn=%s,cn=Users,dc=%s,dc=%s"' %
-                             (aduser, forwardzone.split(".")[0], forwardzone.split(".")[1]),
-                             raiseonerr=False)
-        print ("dsget return code is: %s" % cmd.returncode)
-        print cmd.stdout_text, cmd.stderr_text
-        if cmd.returncode == 1:
-            cmd = ad1.run_command('dsrm "cn=%s,cn=Users,dc=%s,dc=%s" -noprompt' %
-                                 (aduser, forwardzone.split(".")[0], forwardzone.split(".")[1]),
-                                 raiseonerr=False)
-            print ("dsrm return code is: %s" % cmd.returncode)
-            print cmd.stdout_text, cmd.stderr_text
-        cmd = ad1.run_command('dsadd user "cn=%s,cn=Users,dc=%s,dc=%s" -pwd %s -upn %s' %
-                             (aduser, forwardzone.split(".")[0], forwardzone.split(".")[1], aduser_pwd, upn_full),
-                             raiseonerr=False)
-        print ("dsadd return code is: %s" % cmd.returncode)
-        print cmd.stdout_text, cmd.stderr_text
-        if "dsadd succeeded" in cmd.stdout_text:
-            print "AD user add and UPN set successfully"
-        else:
-            pytest.xfail("AD user add and UPN failed")
+        # Restart sssd
+        time.sleep(60)
+        service_control(multihost.master, 'sssd', 'stop')
+        multihost.master.run_command(['rm', '-rf', '/var/lib/sss/{db,mc}/*'], 
+                                     raiseonerr=False)
+        service_control(multihost.master, 'sssd', 'start')
+        time.sleep(60)
         # Check getent passwd command output
         cmd = multihost.master.run_command(['getent', 'passwd', upn_full],
-                                        raiseonerr=False)
+                                           raiseonerr=False)
         print cmd.stdout_text, cmd.stderr_text
         aduser_full = aduser + '@' + forwardzone
         aduser_hd = '/home/' + forwardzone + '/' + aduser
@@ -254,14 +246,14 @@ class TestUPN(object):
             pytest.xfail("Getent passwd with UPN diplaying the actual username and home dir failed")
         # Check id command output
         cmd = multihost.master.run_command(['id', upn_full],
-                                        raiseonerr=False)
+                                           raiseonerr=False)
         aduser_full = aduser + '@' + forwardzone
         if aduser_full in cmd.stdout_text:
             print "Id command with UPN diplays the actual username successfully"
         else:
             pytest.xfail("Id command with UPN diplaying the actual username failed")
         # Check kinit
-        cmd = multihost.master.run_command(['kinit', '-E', upn_full],
+        cmd = multihost.master.run_command('kinit -E ' + upn_full + '@' + forwardzone.upper(),
                                            stdin_text=aduser_pwd,
                                            raiseonerr=False)
         print cmd.stdout_text, cmd.stderr_text
@@ -275,8 +267,8 @@ class TestUPN(object):
             pytest.xfail("Kinit with UPN and shows actual user principal failed")
         # Delete aduser
         ad1.run_command('dsrm "cn=%s,cn=Users,dc=%s,dc=%s" -noprompt' %
-                                        (aduser, forwardzone.split(".")[0], forwardzone.split(".")[1]),
-                                        raiseonerr=False)
+                       (aduser, forwardzone.split(".")[0], forwardzone.split(".")[1]),
+                        raiseonerr=False)
 
         # Cleanup
         multihost.master.kinit_as_admin()
