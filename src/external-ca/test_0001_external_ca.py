18c696cf22c3138f3301f53af9d16c7fc6c7a6bb
Mohammad Rizwan Yusuf <myusuf@redhat.com>
2017-10-16 17:12:36 +0530

    TC- External-CA     Fiexed the existing test case. Added some test cases to validate the server installation
    Change-Id: I1e92aa950f0ead91176164263c7f813756ef815e
    Signed-off-by: Mohammad Rizwan Yusuf <myusuf@redhat.com>

--- src/external-ca/test_0001_external_ca.py
+++ src/external-ca/test_0001_external_ca.py
@@ -5,10 +5,13 @@ import time
 import tempfile
 import pytest
 from ipa_pytests.qe_class import multihost
+from ipa_pytests.qe_install import (disable_firewall, set_hostname,
+                                   set_etc_hosts, set_rngd, print_time)
+from ipa_pytests.shared.rpm_utils import list_rpms
+from ipa_pytests.shared.user_utils import add_ipa_user, show_ipa_user, del_ipa_user
 from ipa_pytests.shared.qe_certutils import certutil
 from ipa_pytests.shared.openssl_utils import openssl_util
 import ipa_pytests.shared.paths as paths
-
 
 class TestExternalCA(object):
     """ Test Class """
@@ -49,6 +52,26 @@ class TestExternalCA(object):
         """
         master = multihost.master
         seconds = 1
+
+        print("Listing RPMS")
+        list_rpms(master)
+        print("Disabling Firewall")
+        disable_firewall(master)
+        print("Setting hostname")
+        set_hostname(master)
+        print("Setting /etc/hosts")
+        set_etc_hosts(master)
+        print("Setting up RNGD")
+        set_rngd(master)
+
+        print_time()
+        print("Installing required packages")
+        master.yum_install(['ipa-server', 'ipa-server-dns',
+                        'bind-dyndb-ldap', 'bind-pkcs11',
+                        'bind-pkcs11-utils'])
+
+        print_time()
+
         cmd = [paths.IPASERVERINSTALL,
                '-p', master.config.admin_pw,
                '-a', master.config.admin_pw,
@@ -114,6 +137,145 @@ class TestExternalCA(object):
             pytest.fail("Failed to install IPA Server using "
                         "--external-cert-file")
         print("Successfully installed IPA Server using --external-ca")
+
+    def test_0002_external_ca_server_install_log_check(self, multihost):
+        """
+
+        :Title: IDM-IPA-TC: Install IPA Server install log check
+
+        :Description: Check if server install logs contians ca subject name
+                      and assert for success message in logs
+
+        :Setup:
+
+        1. RHEL 7.4 system
+
+        :Steps:
+            1. grep "CA_Subject" /var/log/ipaserver-install.log
+            2. tail -1 /var/log/ipaserver-install.log
+        :Expectedresults:
+            1. CA sunject name should be present in logs
+            2. Success message should be present in logs
+
+        :Automation: No
+
+        :CaseComponent: ipa
+
+        """
+        master = multihost.master
+        cmd_arg = ["tail", "-1", "/var/log/ipaserver-install.log"]
+        print("Running : " + str(cmd_arg))
+        cmd = master.run_command(cmd_arg, raiseonerr=False)
+        if cmd.returncode != 0:
+            pytest.fail("Failed : ..!!")
+        assert "The ipa-server-install command was successful" in cmd.stdout_text
+        print("Success : Successfully installed IPA Server")
+
+        #check if log server-install logs contains external ca_subject
+        cmd_arg = ["grep", "Test_CA", "/var/log/ipaserver-install.log"]
+        print("Running : " + str(cmd_arg))
+        cmd = master.run_command(cmd_arg, raiseonerr=False)
+        if cmd.returncode != 0:
+            pytest.fail("Failed : external ca subject not found in /var/log/ipaserver-install.log..!!")
+        print("Success : external ca subject found in /var/log/ipaserver-install.log..!!")
+
+    def test_0003_external_ca_server_install_ipa_services_check(self, multihost):
+        """
+
+        :Title: IDM-IPA-TC: Install IPA Services check after server install
+
+        :Description: Check if ipa services are up and running
+
+        :Setup:
+
+        1. RHEL 7.4 system
+
+        :Steps:
+            1. ipactl status
+        :Expectedresults:
+            1. ipa services should be up and running
+
+        :Automation: No
+
+        :CaseComponent: ipa
+
+        """
+        master = multihost.master
+        #check if ipa services are running after extrnal-ca install
+        cmd_arg = ["ipactl", "status"]
+        print("Running : " + str(cmd_arg))
+        cmd = master.run_command(cmd_arg, raiseonerr=False)
+        if cmd.returncode != 0:
+            pytest.fail("Failed : ipa services not running..!!")
+        print(cmd.stdout_text)
+        print("Success : ipa services are running..!!")
+
+    def test_0004_external_ca_server_install_kinit(self, multihost):
+        master = multihost.master
+        #check if kinit happens
+        passwd = "Secret123"
+        user = "admin"
+        master.run_command(['kinit', user], stdin_text=passwd, raiseonerr=False)
+        cmd = master.run_command('klist')
+        if cmd.returncode != 0:
+            pytest.fail("Failed : kinit failed..!!")
+        print(cmd.stdout_text)
+        print("Success : kinit succeed..!!")
+
+    def test_0005_external_ca_server_install_user_check(self, multihost):
+        """
+
+        :Title: IDM-IPA-TC: Install IPA Server install user check
+
+        :Description: Check if user can be added, deleted and shown
+
+        :Setup:
+
+        1. RHEL 7.4 system
+
+        :Steps:
+            1. ipa user-add
+            2. ipa user-show
+            3. ipa user-del
+        :Expectedresults:
+            1. user should be added
+            2. user should be listed
+            3. user should be deleted
+
+        :Automation: No
+
+        :CaseComponent: ipa
+
+        """
+        master = multihost.master
+        #add ipa user
+        testuser1 = "testuser1"
+        cmd = add_ipa_user(master, testuser1)
+        if cmd.returncode != 0:
+            pytest.fail("Failed : Adding testuser1 failed..!!")
+        print(cmd.stdout_text)
+        print("Success : tesuser1 added successfully..!!")
+
+        #show ipa user
+        cmd = show_ipa_user(master, testuser1)
+        if cmd.returncode != 0:
+            pytest.fail("Failed : testuser1 not found..!!")
+        print(cmd.stdout_text)
+        print("Success : tesuser1 found..!!")
+
+        #del ipa user
+        cmd = del_ipa_user(master, testuser1)
+        if cmd.returncode != 0:
+            pytest.fail("Failed : testuser1 deletion failed..!!")
+        print(cmd.stdout_text)
+        print("Success : tesuser1 deleted..!!")
+
+        #show ipa user
+        cmd = show_ipa_user(master, testuser1)
+        if cmd.returncode == 0:
+            pytest.fail("Failed : testuser1 found after deletion..!!")
+        print(cmd.stdout_text)
+        print("Success : tesuser1 not found..!!")
 
     def class_teardown(self, multihost):
         """ Teardown for class """
