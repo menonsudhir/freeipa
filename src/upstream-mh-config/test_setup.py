96107cd6c0eb039e1aca4c03f60ccb80d70c2384
Kaleemullah Siddiqui <ksiddiqu@redhat.com>
2018-05-14 09:57:33 +0530

    Adding no strictcheck option for ssh
    Change-Id: I2b3b0b36be5827294735e99d8b5ed76020e4c65f
    Signed-off-by: Kaleemullah Siddiqui <ksiddiqu@redhat.com>

--- src/upstream-mh-config/test_setup.py
+++ src/upstream-mh-config/test_setup.py
@@ -11,13 +11,21 @@ with open((sys.argv[1:])[0]) as stream:
     try:
         code = yaml.load(stream)
         hosts = code['domains'][0]['hosts']
-        for host in hosts:
-           #print(host['ip'],host['name'])
-           etchostname = "echo "+host['name']+" > /etc/hostname"
+        no_of_hosts = len(hosts)
+        for i in range(no_of_hosts):
+           etchostname = "echo "+hosts[i]['name']+" > /etc/hostname"
+           subprocess.call(["ssh", "-o StrictHostKeyChecking=no", "root@"+hosts[i]['ip'], etchostname])
            print(etchostname)
-           etchost = "echo "+host['ip']+" "+host['name']+" > /etc/hosts"
-           print(etchost)
-           subprocess.call(["ssh", "root@"+host['ip'], etchostname])
-           subprocess.call(["ssh", "root@"+host['ip'], etchost])
+           cmd = []
+           for j in range(no_of_hosts):
+             if j is 0:
+               cmd.append("echo "+hosts[j]['ip']+" "+hosts[j]['name']+" > /etc/hosts")
+             else:
+               cmd.append("echo "+hosts[j]['ip']+" "+hosts[j]['name']+" >> /etc/hosts")
+           print(cmd)
+           for k in range(no_of_hosts):
+             print(cmd[k])
+             subprocess.call(["ssh", "-o StrictHostKeyChecking=no", "root@"+hosts[i]['ip'], cmd[k]])
     except yaml.YAMLError as exc:
         print(exc)
+
