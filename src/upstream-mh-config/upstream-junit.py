a097632d157fafbb0304e76daed7cae2714906d8
Kaleemullah Siddiqui <ksiddiqu@redhat.com>
2018-05-25 18:55:11 +0530

    script to modify upstream junit.xml
    Change-Id: I14be8608602030d890573253cd20cc43368f5f2e
    Signed-off-by: Kaleemullah Siddiqui <ksiddiqu@redhat.com>

--- src/upstream-mh-config/upstream-junit.py
+++ src/upstream-mh-config/upstream-junit.py
@@ -10,4 +10,4 @@ for testcase in root.findall('testcase'):
   name = "IDM-IPA-TC : " + test_suite + " : "+ testcase.get('name')
   print name
   testcase.set('name', name)
-tree.write(sys.argv[1:][0])
+tree.write(sys.argv[1:][0], encoding="utf-8", xml_declaration=True)
