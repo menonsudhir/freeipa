b2aed93e817bfd81cddc1460f2127cd65bfa5b03
Kaleemullah Siddiqui <ksiddiqu@redhat.com>
2018-05-25 16:47:22 +0530

    Modifying junit.xml for upstream tests for polarion
    Change-Id: Ica811c0a982f71e5337f8204e812bf70bf0ce7a8
    Signed-off-by: Kaleemullah Siddiqui <ksiddiqu@redhat.com>

---
+++ src/upstream-mh-config/upstream-junit.py
@@ -0,0 +1,13 @@
+import os
+import sys 
+import xml.etree.ElementTree as ET
+
+test_suite =  os.environ['TEST_SUITE'].split("/")[-1].split(".")[0]
+tree = ET.parse(sys.argv[1:][0])
+
+root = tree.getroot()
+for testcase in root.findall('testcase'):
+  name = "IDM-IPA-TC : " + test_suite + " : "+ testcase.get('name')
+  print name
+  testcase.set('name', name)
+tree.write(sys.argv[1:][0])
