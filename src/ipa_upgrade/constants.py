a58f827b6b14f9bb6eaa5bcde4e7e2f1efa7f55f
Anuja More <amore@redhat.com>
2018-06-28 12:07:15 +0530

    Repo for 7.6
    Patch1: Repo for 7.6 is added in
    	src/ipa_upgrade/constants.py
    
    Change-Id: Iea5892a18fe9ed15906fb252a3c15c873f1f0edb
    Signed-off-by: Anuja More <amore@redhat.com>

--- src/ipa_upgrade/constants.py
+++ src/ipa_upgrade/constants.py
@@ -16,5 +16,6 @@ repo_urls = {
     "7.5.0": ["http://download-node-02.eng.bos.redhat.com/rel-eng/updates/RHEL-7.5/RHEL-7.5-Update-0.0/compose/Server/x86_64/os/"],
     #"7.5.a": ["http://download.eng.pnq.redhat.com/rel-eng/repos/rhel-7.5-candidate/x86_64/"],
     "7.5.a": ["http://download-node-02.eng.bos.redhat.com/rel-eng/updates/RHEL-7.5/RHEL-7.5-Update-1.1/compose/Server/x86_64/os/"],
-    "7.5.b": ["http://download.eng.pnq.redhat.com/rel-eng/repos/rhel-7.5-z-candidate/x86_64/"]           # 7.5z repo
+    "7.5.b": ["http://download.eng.pnq.redhat.com/rel-eng/repos/rhel-7.5-z-candidate/x86_64/"],           # 7.5z repo
+    "7.6.a": ["http://download-node-02.eng.bos.redhat.com/rel-eng/repos/rhel-7.6-candidate/x86_64/"]
 }
