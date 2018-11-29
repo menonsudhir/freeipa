6c5356688ae9895a3a35c69304fdbf38d4abb9ac
Anuja More <amore@redhat.com>
2018-11-29 11:36:02 +0530

    pytest :- ipa-rbac: fix show permission with rights
    Problem: - A test is failing on RHEL8: "show permission with rights"
    - test failing on searching incorrect attributelevelrights
    
    Fix :
    - correct strings to check for attributelevelrights
    in a loop so that they are all checked but order is not fixed.
    
    Patch 2 :
    -Fixes added for indentation.
    
    Patch 3 :
    -check1.returncode check is removed thus check1
     is removed as it is unused declaration.
    -for string concatination .format() is used.
    -assert is used instead of if else.
    
    Change-Id: Iebe53617389190474ae9cd0349c8703ba3be16a3
    Signed-off-by: Anuja More <amore@redhat.com>

--- src/ipa-rbac/test_permission_show.py
+++ src/ipa-rbac/test_permission_show.py
@@ -18,33 +18,26 @@ class TestPermissionShowPositive(object):
         IDM-IPA-TC : rbac : Function to show permission with rights
         """
         permission_name = "ManageUser"
-        exp_output = "attributelevelrights: {u'ipapermright': u'rscwo', u'cn': u'rscwo', " \
-                     "u'ipapermtarget': u'rscwo', u'ipapermtargetto': u'rscwo', u'attrs': " \
-                     "u'rscwo', u'ipapermlocation': u'rscwo', u'ipapermincludedattr': u'rscwo', " \
-                     "u'nsaccountlock': u'rscwo', u'ipapermtargetfrom': u'rscwo', " \
-                     "u'ipapermbindruletype': u'rscwo', u'owner': u'rscwo', " \
-                     "u'member': u'rscwo', u'type': u'rscwo', u'ipapermtargetfilter': u'rscwo', " \
-                     "u'description': u'rscwo', u'businesscategory': u'rscwo', u'memberof': " \
-                     "u'rscwo', u'seealso': u'rscwo', u'ipapermissiontype': u'rscwo', " \
-                     "u'objectclass': u'rscwo', u'aci': u'rscwo', u'o': u'rscwo', " \
-                     "u'ipapermdefaultattr': u'rscwo', u'ou': u'rscwo', u'targetgroup': " \
-                     "u'rscwo', u'ipapermexcludedattr': u'rscwo'}"
+
+        rights = ['objectclass', 'aci', 'cn', 'member', 'businesscategory', 'seealso', 'owner',
+                  'ou', 'o', 'description', 'ipapermissiontype', 'ipapermbindruletype',
+                  'ipapermlocation', 'ipapermdefaultattr', 'ipapermincludedattr',
+                  'ipapermexcludedattr', 'ipapermright', 'ipapermtargetfilter', 'ipapermtarget',
+                  'ipapermtargetto', 'ipapermtargetfrom', 'nsaccountlock', 'targetgroup',
+                  'memberof', 'type', 'attrs']
+
         multihost.master.kinit_as_admin()
-        check1 = permission_add(multihost.master, permission_name, ['--setattr=owner=cn=test',
-                                                                    '--addattr=owner=cn=test2',
-                                                                    '--right=read',
-                                                                    '--right=write',
-                                                                    '--attr=carlicense',
-                                                                    '--attr=description',
-                                                                    '--type=user'])
-        if check1.returncode == 0:
-            print("Permission " + permission_name + " added successfully")
-            check1a = permission_show(multihost.master, permission_name, ['--all',
-                                                                          '--rights'])
-            if exp_output in check1a.stdout_text:
-                print("Success! permission-show works as expected")
-            else:
-                pytest.xfail("Error occured in this test")
-            permission_del(multihost.master, permission_name)
-        else:
-            pytest.xfail("Failed to add permission")
+        permission_add(multihost.master, permission_name, ['--setattr=owner=cn=test',
+                                                           '--addattr=owner=cn=test2',
+                                                           '--right=read',
+                                                           '--right=write',
+                                                           '--attr=carlicense',
+                                                           '--attr=description',
+                                                           '--type=user'])
+        print("Permission {} added successfully".format(permission_name))
+        check1a = permission_show(multihost.master, permission_name, ['--all', '--rights'])
+        for attr in rights:
+            value = "'{}': 'rscwo'".format(attr)
+            assert value in check1a.stdout_text
+
+        permission_del(multihost.master, permission_name)
