891f3c6974d0779af476c458a1ade8fa059accc9
Xiyang Dong <xdong@redhat.com>
2018-12-06 11:33:39 -0500

    FREEIPA-2231 Fix python3 print issue
    Change-Id: I0694abfac86f580160aa53cb13181b34a80de52c

--- src/upstream-mh-config/upstream-junit.py
+++ src/upstream-mh-config/upstream-junit.py
@@ -1,5 +1,5 @@
 import os
-import sys 
+import sys
 import xml.etree.ElementTree as ET
 
 test_suite =  os.environ['TEST_SUITE'].split("/")[-1].split(".")[0]
@@ -8,7 +8,7 @@ tree = ET.parse(sys.argv[1:][0])
 root = tree.getroot()
 for testcase in root.findall('testcase'):
   name = "IDM-IPA-TC : " + test_suite + " : "+ testcase.get('name')
-  print name
+  print(name)
   testcase.set('name', name)
 tree.write(sys.argv[1:][0])
 xml_str = ('<?xml version="1.0" encoding="utf-8"?>' + ET.tostring(tree.getroot()))
